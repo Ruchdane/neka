639e43d50dc5a61988aa7d9d74561bf4b0bdde74
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2023-09-05 08:56:33 +0100

    Setup spa inside aspnet minimal api

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,20 +3,30 @@ using Backend.db.dao;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapFallbackToFile("index.html");
+
 
 using var db = new AppDbContext();
 Console.WriteLine($"Database path: {db.DbPath}.");
 
 var accountDao = new AccountDao(db);
 
-app.MapGet("/create", () =>
+app.MapGet("/api/create", () =>
 {
     var account = new Backend.db.model.Account { profile = "Student", Username = "Test", hashPassword = "^ddoidnoandoanzaio" } ;
     accountDao.Insert(account);
     return "";
 });
 
-app.MapGet("/", () =>
+app.MapGet("/api/get", () =>
 {
     var account  = accountDao.SelectByUsername("Test");
     return account.hashPassword;
